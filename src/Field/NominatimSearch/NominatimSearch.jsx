c639bdef82e38bce0a21de889ed3aef75499c6dc
arieger <rieger@terrestris.de>
2018-04-26 14:31:00 +0200

    use named import for transformExtent function of ol/proj

--- src/Field/NominatimSearch/NominatimSearch.jsx
+++ src/Field/NominatimSearch/NominatimSearch.jsx
@@ -5,7 +5,7 @@ const Option = AutoComplete.Option;
 
 import Logger from '../../Util/Logger';
 import UrlUtil from '../../Util/UrlUtil/UrlUtil';
-import olProj from 'ol/proj';
+import { transformExtent } from 'ol/proj';
 
 import { CSS_PREFIX } from '../../constants';
 
@@ -150,7 +150,7 @@ export class NominatimSearch extends React.Component {
           return parseFloat(coord);
         });
 
-        extent = olProj.transformExtent(extent, 'EPSG:4326',
+        extent = transformExtent(extent, 'EPSG:4326',
           olView.getProjection().getCode());
 
         olView.fit(extent, {
